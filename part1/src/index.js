14a5636611b9df4e880249f05b5debbe14169b26
didierrevelo <1829@holbertonschool.com>
2021-12-12 01:32:03 -0500

    change of variables

--- part1/src/index.js
+++ part1/src/index.js
@@ -29,23 +29,33 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+    exercises: 10
+    },
+    {
+    name: 'Using props to pass data',
+    exercises: 7
+    },
+    {
+    name: 'State of a component',
+    exercises: 14
+    }
+  ]
   return (
+    
     <div>
       <Header course={course} />
-      <Content part1={part1}
-      exercises1={exercises1}
-      part2={part2}
-      exercises2={exercises2}
-      part3={part3}
-      exercises3={exercises3} />
-      <Total  exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Content 
+      part1 = {parts[0].name}
+      exercises1={parts[0].exercises}
+      part2 = {parts[1].name}
+      exercises2={parts[1].exercises}
+      part3 = {parts[2].name}
+      exercises={parts[2].exercises}
+      />
+      <Total  exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises}/>
     </div>
   )
 }
